73d1c6f5fec6e9550171bb0d831624e1417a195a
Bloodenela <135967034+Bloodenela@users.noreply.github.com>
2023-07-09 22:32:54 +0300

    Delete summerPracticeSSTU directory

--- summerPracticeSSTU/demo/src/main/java/java/com/ryasnov/transactionservice/entities/Role.java
+++
@@ -1,6 +0,0 @@
-package java.com.ryasnov.transactionservice.entities;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- summerPracticeSSTU/demo/src/main/java/java/com/ryasnov/transactionservice/services/TransactionServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package java.com.ryasnov.transactionservice.services;
-
-public class TransactionServiceImpl {
-}
