a89ab2c13c22cab28e6860af0fd772d560b3a83c
Bloodenela <135967034+Bloodenela@users.noreply.github.com>
2023-07-09 23:11:25 +0300

    Add files via upload
    Preversion of TransactionService

---
+++ Transaction-service/src/main/java/com/ryasnov/transactionservice/entities/TypeTransaction.java
@@ -0,0 +1,10 @@
+package com.ryasnov.transactionservice.entities;
+
+public enum TypeTransaction {
+    PURCHASE,
+    SALE,
+    ADD_TO_SUBSCRIPTIONS,
+    REMOVING_FROM_SUBSCRIPTIONS,
+    WITHDRAWAL,
+    PUT_ON_THE_ACCOUNT
+}
