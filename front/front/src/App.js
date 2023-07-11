c1f2d0019d5625875df450a2fb013f551f3851df
o1egs <tankistonline21@gmail.com>
2023-07-11 09:47:38 +0400

    добавил всё что сделал

--- front/front/src/App.js
+++ front/front/src/App.js
@@ -1,14 +1,22 @@
 import Search from "./components/Search/Search";
 import IndexAccount from "./components/IndexAccount/IndexAccount";
 import AccountStock from "./components/AccountStock/AccountStock";
+import Header from "./components/Header/Header";
+import styles from "./App.module.css"
+import MainPage from "./pages/MainPage/MainPage";
+import LoginForm from "./components/LoginForm/LoginForm";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import {Route, Routes} from "react-router-dom";
+import Stock from "./components/Stock/Stock";
 
 
 function App() {
   return (
-    <div className="App">
-        {/*<Search />*/}
-        <IndexAccount />
-        {/*<AccountStock />*/}
+    <div className={styles.app}>
+        {/*<Routes>*/}
+        {/*    <Route path={"/"} element={<MainPage />}/>*/}
+        {/*</Routes>*/}
+        <Stock />
     </div>
   );
 }
