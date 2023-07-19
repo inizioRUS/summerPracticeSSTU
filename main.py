0edbcdf2ad965608aeea4ecb557fdc25ba0d5eed
inizioRUS <garanin203@bk.ru>
2023-07-19 23:38:30 +0400

    off cors

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from Predicter import Predicter
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 predicter = Predicter()
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -15,6 +28,7 @@ async def root():
 @app.get("/predict/{name}")
 async def say_hello(name: str):
     return predicter.predict(name)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
