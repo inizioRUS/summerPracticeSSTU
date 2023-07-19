e64440ce1ae625f294c49b5d0093710325338bdb
inizioRUS <garanin203@bk.ru>
2023-07-19 16:34:27 +0400

    add unicorn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from Predicter import Predicter
+import uvicorn
 
 app = FastAPI()
 
@@ -14,3 +15,6 @@ async def root():
 @app.get("/predict/{name}")
 async def say_hello(name: str):
     return predicter.predict(name)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
