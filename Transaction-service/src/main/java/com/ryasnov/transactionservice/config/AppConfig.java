d60de519867517e150df30bd9029c53613a91389
Blooden <pashpavel217@gmail.com>
2023-07-20 20:09:53 +0300

    Fix bag

--- Transaction-service/src/main/java/com/ryasnov/transactionservice/config/AppConfig.java
+++ Transaction-service/src/main/java/com/ryasnov/transactionservice/config/AppConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,6 +23,10 @@ public class AppConfig {
     private UserRepository userRepository;
     @Autowired
     private TransactionRepository transactionRepository;
+    @Bean
+    public WebClient localApiClient() {
+        return WebClient.create("http://80.249.145.114:8000/today/");
+    }
 
 
 }
